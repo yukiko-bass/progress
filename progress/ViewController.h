b50455053e2bcfe9c83f8b372a10df14bd7086d8
CatenaRentalSystem <catenarentalsystem@CatenaRentalSystem-no-MacBook-Pro.local>
2013-04-19 11:15:46 +0900

    Initial Commit

---
+++ progress/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  progress
+//
+//  Created by CatenaRentalSystem on 13/04/19.
+//  Copyright (c) 2013年 CatenaRentalSystem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ progress/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  progress
+//
+//  Created by CatenaRentalSystem on 13/04/19.
+//  Copyright (c) 2013年 CatenaRentalSystem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
